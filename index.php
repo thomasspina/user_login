b65ad16cbe62ddb5e3dd134bb5deea883d281a27
Thomas Spina <thomas@thomasspina.com>
2021-02-05 19:56:35 -0500

    added index via upload
    initial commit. cli is bugging

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>login</title>
+</head>
+<div class="header">
+    <h3>please login</h3>
+</div>
+<div class="login">
+    <form>
+        <label for="id">Username/Email:</label>
+        <input type="text" id="id"><br>
+        <label for="password">Password:</label>
+        <input type="text" id="password"><br>
+        <input type="submit" value="Login">
+    </form>
+</div>
+
+</html>
