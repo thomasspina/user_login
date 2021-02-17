3e690907579ebc556e81fc1de424d9a6c3adc13d
Thomas <thomas@thomasspina.com>
2021-02-16 23:05:23 -0500

    added a function to check identifier validity. Also checked password and password confirmation validity

--- register.php
+++ register.php
@@ -5,24 +5,25 @@ include_once "config.php";
 $email = $username = $password = $confirm_password = "";
 $email_err = $usernam_err = $password_err = $confirm_password_err = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty(trim($_POST["email"]))) {
-        $email_err = "Please enter an email address.";
+// checks if a certain identifier (username / email) is valid
+function checkIdentifierValidity($var, $err, $name, $link) {
+    if (empty(trim($_POST[$name]))) {
+        $err = "Please enter a valid input.";
     } else {
-        $sql = "SELECT id FROM users WHERE email = ?"; // ? is a placeholder
+        $sql = "SELECT id FROM users WHERE {$name} = ?"; // ? is a placeholder
         
         if ($stmt = mysqli_prepare($link, $sql)) { // prepares a statement to use on db, returns false if error
-            mysqli_stmt_bind_param($stmt, "s", $param_email); // binds parameters to the prepared statement
+            mysqli_stmt_bind_param($stmt, "s", $param); // binds parameters to the prepared statement
 
-            $param_email = trim($_POST["email"]);
+            $param = trim($_POST[$name]);
 
             if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt); // stores results client side
 
                 if (mysqli_stmt_num_rows($stmt) == 1) {
-                    $email_err = "There already exists an account with that email.";
+                    $err = "This is already in use by another account.";
                 } else {
-                    $email = trim($_POST["email"]);
+                    $var = trim($_POST[$name]);
                 }
             } else {
                 echo "Oops! Something went wrong. Please try again later.";
@@ -30,33 +31,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             mysqli_stmt_close($stmt);
         }
     }
+}
 
-    // TODO find a way to combine the statement below with the one above. Make a function or something. Seems redundant
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    checkIdentifierValidity($email, $email_err, "email", $link);
+    checkIdentifierValidity($username, $usernam_err, "username", $link);
 
-    if (empty(trim($_POST["username"]))) {
-        $usernam_err = "Please enter a username.";
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
+        $password_err = "Your password must have at least 6 characters.";
+    } elseif (is_numeric(trim($_POST["password"])) || 1 !== preg_match('~[0-9]~', trim($_POST["password"]))) {
+        $password_err = "Your password must contain both letters and numbers.";
     } else {
-        $sql = "SELECT id FROM users WHERE username = ?"; 
-        
-        if ($stmt = mysqli_prepare($link, $sql)) { 
-            mysqli_stmt_bind_param($stmt, "s", $param_username); 
+        $password = trim($_POST["password"]);
+    }
 
-            $param_username = trim($_POST["username"]);
-
-            if (mysqli_stmt_execute($stmt)) {
-                mysqli_stmt_store_result($stmt);
-
-                if (mysqli_stmt_num_rows($stmt) == 1) {
-                    $usernam_err = "There already exists an account with that username.";
-                } else {
-                    $usernam_err = trim($_POST["username"]);
-                }
-            } else {
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-            mysqli_stmt_close($stmt);
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($password_err) && ($confirm_password != $password)) {
+            $confirm_password_err = "Password did not match.";
         }
     }
+
+
+    
 }
 
 ?>
